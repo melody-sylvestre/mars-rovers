78559b096060d9c863d1cbe62b5d8f10d362a2c9
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-08-26 16:48:01 +0100

    validation of positions and instructions format OK

--- src/moveMarsRovers.ts
+++ src/moveMarsRovers.ts
@@ -25,8 +25,43 @@ const moveMarsRovers = (request: Request, response: Response) => {
     statusCode = 400
   } else {
     const inputCommands: roversCommands = commandsParser(input)
-    output.message = 'OK'
-    console.log(inputCommands)
+    if (!validateUpperRightCoordinates(inputCommands.upperRightCoordinates)) {
+      output.message = "Incorrect input format: upperRightCoordinates should be 2 strictly positive numbers (e.g. '5 5')"
+      statusCode = 400
+    
+    } else {
+      const maxPositionX = Number(inputCommands.upperRightCoordinates.split(' ')[0])
+      const maxPositionY = Number(inputCommands.upperRightCoordinates.split(' ')[1])
+      const numberOfRovers: number = inputCommands.roverPositions.length
+      let foundInvalidPosition: boolean = false
+      let foundInvalidInstructions: boolean = false
+
+      for (let i = 0; i < numberOfRovers; i++) {
+        // validate Rovers Positions format
+        if (!validateRoverPosition(inputCommands.roverPositions[i], maxPositionX, maxPositionY)) {
+          statusCode = 400
+          foundInvalidPosition = true
+          output.message = `${output.message}Rover #${i + 1}: Incorrect position. `
+        }
+        
+        if (!validateRoverInstructions(inputCommands.roverInstructions[i])) {
+          statusCode = 400
+          foundInvalidInstructions = true
+          output.message = `${output.message}Rover #${i + 1}: Incorrect instructions. `
+        }  
+      }
+
+      if( foundInvalidPosition ){
+        output.message = `${output.message}Rover positions must have the format X Y [NSWE] (e.g '1 3 N') with 0 <= X <= ${maxPositionX} and  0 <= Y <= ${maxPositionY}. Both number must be integers. `
+      }
+      if( foundInvalidInstructions ){
+       output.message = `${output.message}Rover instructions must be an empty string or contains only L, M or R. `
+      }
+
+      // validate rovers positions against collisions -> check duplicates
+      if( !foundInvalidPosition && !foundInvalidInstructions )
+        output.message = 'OK'
+    }
   }
   response.status(statusCode).json(output)
 }
