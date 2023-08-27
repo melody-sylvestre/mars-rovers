8d0cded3e6fc134001250e5f2dab51f28b6ab8d4
Melody Sylvestre <melody.sylvestre@bristol.ac.uk>
2023-08-27 11:37:34 +0100

    formatting

--- __test__/roversMotions.test.ts
+++ __test__/roversMotions.test.ts
@@ -40,56 +40,56 @@ describe("Testing that rotate instructions (L or R) are correctly executed", () 
     })
     test('Rotating Right from W', () => {
         expect(rotate('2 3 W', 'R')).toBe('2 3 N')
-    })       
+    })
 })
 
 describe('Checking that instructions to a rover can be correctly executed (executeRoversInstructions)', () => {
     test('Moving a single rover in a straight line within the limit of the plateau', () => {
-        const expectedResult : RoverStatus = {
+        const expectedResult: RoverStatus = {
             roverPositions: ['0 5 N'],
-            message: 'Instructions complete. ', 
+            message: 'Instructions complete. ',
             instructionsComplete: true
         }
-        expect(executeRoverInstructions(0, ['0 0 N'],'MMMMM', 5, 5)).toEqual(expectedResult)
+        expect(executeRoverInstructions(0, ['0 0 N'], 'MMMMM', 5, 5)).toEqual(expectedResult)
     })
     test('Trying to move a single rover in a straight line beyond the limit of the plateau', () => {
-        const expectedResult : RoverStatus = {
+        const expectedResult: RoverStatus = {
             roverPositions: ['1 3 N'],
-            message: 'Reached the limit of the plateau. Stopping the execution of the instructions. ', 
+            message: 'Reached the limit of the plateau. Stopping the execution of the instructions. ',
             instructionsComplete: false
         }
-        expect(executeRoverInstructions(0, ['1 3 N'],'MMMMMMM', 5, 5)).toEqual(expectedResult)
+        expect(executeRoverInstructions(0, ['1 3 N'], 'MMMMMMM', 5, 5)).toEqual(expectedResult)
     })
     test('Moving a single rover in a L-shape', () => {
-        const expectedResult : RoverStatus = {
+        const expectedResult: RoverStatus = {
             roverPositions: ['4 1 E'],
-            message: 'Instructions complete. ', 
+            message: 'Instructions complete. ',
             instructionsComplete: true
         }
-        expect(executeRoverInstructions(0, ['1 5 N'],'RRMMMMLMMM', 10, 20)).toEqual(expectedResult)
+        expect(executeRoverInstructions(0, ['1 5 N'], 'RRMMMMLMMM', 10, 20)).toEqual(expectedResult)
     })
     test('One rover but no instructions - staying still', () => {
-        const expectedResult : RoverStatus = {
+        const expectedResult: RoverStatus = {
             roverPositions: ['3 17 W'],
-            message: 'Instructions complete. ', 
+            message: 'Instructions complete. ',
             instructionsComplete: true
         }
-        expect(executeRoverInstructions(0, ['3 17 W'],'', 30, 40)).toEqual(expectedResult)
+        expect(executeRoverInstructions(0, ['3 17 W'], '', 30, 40)).toEqual(expectedResult)
     })
     test('Moving the 2nd rover out of a squad of 3', () => {
-        const expectedResult : RoverStatus = {
+        const expectedResult: RoverStatus = {
             roverPositions: ['2 10 W', '1 15 S', '13 14 E'],
             message: 'Instructions complete. ',
             instructionsComplete: true
         }
-        expect(executeRoverInstructions(1, ['2 10 W', '1 17 N', '13 14 E'],'LLMM', 20, 20)).toEqual(expectedResult)
+        expect(executeRoverInstructions(1, ['2 10 W', '1 17 N', '13 14 E'], 'LLMM', 20, 20)).toEqual(expectedResult)
     })
     test('Moving a rover to collide with another', () => {
-        const expectedResult : RoverStatus = {
+        const expectedResult: RoverStatus = {
             roverPositions: ['3 17 W', '1 17 S'],
             message: 'Another rover is in the way. Stopping the execution of the instructions. ',
             instructionsComplete: false
         }
-        expect(executeRoverInstructions(0, ['3 17 W', '1 17 S'],'MMM', 30, 40)).toEqual(expectedResult)
+        expect(executeRoverInstructions(0, ['3 17 W', '1 17 S'], 'MMM', 30, 40)).toEqual(expectedResult)
     })
 })
